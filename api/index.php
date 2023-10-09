0161230eb79fb1958720b8bb92562ec0b4708a00
Estevao Leite <estevaotlnf@gmail.com>
2023-10-09 19:43:15 -0300

    Update: correções de caminhos relativos na api

--- api/index.php
+++ api/index.php
@@ -24,7 +24,7 @@ try {
     // });
 
     $apiInicial = "";
-    if (strpos($referer, 'localhost') !== false) {
+    if (strpos($_SERVER['HTTP_REFERER'], 'localhost') !== false) {
         $apiInicial = "/match-servicos/api";
     }
 
