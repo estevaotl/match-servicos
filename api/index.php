ac88bf500902e2fa6223c0df94025fa491ea6730
Estevao Leite <estevaotlnf@gmail.com>
2023-10-10 20:32:51 -0300

    Update: cors na api

--- api/index.php
+++ api/index.php
@@ -1,10 +1,13 @@
 <?php
 // Configuração do CORS
-// header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Credentials: true");
-// header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
-// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header("HTTP/1.1 200 OK");
+    die();
+}
 
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
